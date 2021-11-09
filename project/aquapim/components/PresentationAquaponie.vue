552c78cc5a87403b5e47cf3aaeb00c4020cebb46
felix <felix.portier@epitech.eu>
2021-11-09 15:56:39 +0100

    Page Aquaponie

--- project/aquapim/components/PresentationAquaponie.vue
+++ project/aquapim/components/PresentationAquaponie.vue
@@ -4,8 +4,28 @@
             <h1 class="mx-auto">L'AQUAPONIE</h1>
         </b-row>
 
-        <b-row> 
-            <img class="mx-auto" src="../static/schema.gif" width="50%" />
+        <b-row class="mx-auto schema mt-3">
+
+
+            <b-col class="mt-5 col1">
+                <p>0 changement d'eau <br/>
+                    0 intrant chimique <br/>
+                    Autonomie = Peu d'entretien
+                </p>
+            </b-col>
+
+            <b-col >
+                <b-img id="schema-gif" class="mx-auto" src="../static/schema.gif"  fluid-grow />
+            </b-col>
+
+            <b-col class="mt-5 col2">
+                <p>
+                    Parlement Européen: <br/>
+                    "Une des 10 technologies qui pourraient changer nos vies"
+                </p>
+            </b-col>
+
+            
         </b-row>
         
     </b-container>
@@ -16,6 +36,26 @@
 h1{
     font-family: "Poppins-Bold";
     color:#3e602f;
+}
+
+.col1{
+    border-top-style: groove;
+    border-left-style: groove;
+}
+
+.col2{
+    border-bottom: groove;
+    border-right: groove;
+}
+
+.schema {
+    
+    font-family: "Poppins-Medium";
+    font-size: 20px;
+}
+
+#schema-gif {
+    width: 500%;
 }
 
 </style>
