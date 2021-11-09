3c3b06d62a404142d3d1984291137ba36af6bba8
bleyerguillaumeepitech <90611119+bleyerguillaumeepitech@users.noreply.github.com>
2021-11-09 17:01:41 +0100

    Merge sam

--- project/aquapim/components/Concept.vue
+++ project/aquapim/components/Concept.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="col-12" id="margtop">
-      <div class="col-12 d-flex justify-content-center mt-4 pb-3">
-        <!-- MODIFICATION TITRE -->
-        <p id="title">NOTRE CONCEPT</p>
-      </div>
-
-      <div class="col-10 offset-1 d-flex justify-content-center mt-2">
-        <!-- MODIFICATION IMAGE (modifier la destination de l'imgage dans src="") -->
-        <img class="img-fluid" src="../static/Concept.png" >
-      </div>
+  <div class="col-12" id="margtop">
+    <div class="col-12 d-flex justify-content-center mt-4 pb-3">
+      <!-- MODIFICATION TITRE -->
+      <p id="title">NOTRE CONCEPT</p>
     </div>
 
+    <div class="col-12 col-xl-10 offset-xl-1 d-flex justify-content-center mt-5">
+      <!-- MODIFICATION IMAGE (modifier la destination de l'imgage dans src="") -->
+      <img class="img-fluid" src="../static/Concept.png" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 
 @font-face {
-    font-family: "Poppins-bold";
-    src: local("Poppins-bold"),     url(../static/font/Poppins-Bold.ttf) format("truetype");
+  font-family: 'Poppins-bold';
+  src: local('Poppins-bold'),
+    url(../static/font/Poppins-Bold.ttf) format('truetype');
 }
-
-
 
 @media all and (min-width: 319px) {
   #title {
@@ -44,5 +42,4 @@
   margin-top: 10%;
 }
 }
-
 </style>
